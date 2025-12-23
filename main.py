7b60e8ed8541416f065f197b4730612097823e87
Meo2468 <marvinogbeta2@gmail.com>
2025-12-23 02:07:59 +0100

    Initial commit: on-screen JP Keyboard

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+# Create the main application window.
+window = tk.Tk()
+
+# Declaring the window's title and size.
+window.title("JP_KeyBoard")
+window.geometry("1350x300")
+
+
+
+window.mainloop() # Start the GUI event loop.
